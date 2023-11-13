e373577900671170dcf1f5beff34d66e0bfa8045
yunanchang <asdqscesz70731@yahoo.com.tw>
2023-11-13 11:30:53 +0800

    1113

---
+++ reg.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>會員註冊</h2>
+    <form action="">
+        <div>
+            <label>密碼</label>
+            <input type="text" name='address' id='address' >
+        </div>
+        <div>
+            <label>帳號</label>
+            <input type="text" name='address' id='address' >
+        </div>
+    </form>
+</body>
+</html>
